71cf7ca6e0eda9a767aa5298b0bb3613c0b314d6
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2022-02-15 11:32:55 +0000

    Add files via upload

---
+++ enquires.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head> 
+    <title>Enquiry Sent </title>
+</head>
+Hello <?php echo $_POST["name"]; ?> 
+<?php echo "Thank you for your enquires :).";?>
+</body>
+</html>
---
+++ inputUsr3.js
@@ -0,0 +1,5 @@
+const name = window.prompt("Enter your name: ", "Name");
+const birthday = window.prompt("Enter your birthday: " ,"Day.Month");
+
+
+alert(`Welcome ${name} to Sweet Shop by Sarah Admin and You will have your birthday offer on ${birthday}`);
